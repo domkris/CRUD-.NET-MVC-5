c8eda2d6d339c7241ba9255fcf433ed41bfa6070
Domagoj Kristov <dkrist00@fesb.hr>
2018-09-27 18:26:11 +0200

    SQL database

---
+++ VersionSQL/DESKTOP-QOB3KOV/SQLEXPRESS/myDB/dbo/Table/Category.sql
@@ -0,0 +1,13 @@
+/****** Object:  Table [dbo].[Category]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[Category](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nchar](100) NULL,
+ CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
